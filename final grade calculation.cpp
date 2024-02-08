bbc0b94abbf1120044e414d199228471a94d4a79
Ateto1202 <tonysu1204@gmail.com>
2024-02-08 11:30:37 +0800

    Complete wrapping task

--- final grade calculation.cpp
+++ final grade calculation.cpp
@@ -12,93 +12,9 @@ struct Node {
 };
 
 
-bool calResult(const int course, const double score_sum, const double point_sum) {
-
-    if(course > 0) {
-
-        cout << "course amount: " << course << endl;
-        cout << "total point: " << point_sum << endl;
-
-        double avg;
-        if(point_sum == 0) avg = 0;
-        else avg = score_sum / point_sum;
-
-        cout << "avg: " << avg << endl;
-
-        return true;
-
-    }
-
-    return false;
-}
 
 
-bool travel_node(struct Node *head) {
-
-    struct Node *ptr = head->next;
-    bool isEmpty = true;
-
-
-    while(ptr) {
-
-        if(isEmpty) {
-            cout << "-> Current grade list: " << endl;
-        }
-        isEmpty = false;
-
-        cout << ptr->score << ' ' << ptr->point << endl;
-        ptr = ptr->next;
-
-    }
-
-    return isEmpty;
-
-}
-
-
-void add_node(struct Node *head, const double score, const double point) {
-
-    struct Node *cur = new Node;
-    cur->score = score;
-    cur->point = point;
-    cur->next = NULL;
-
-    if(head->next) {
-
-        struct Node *pre = head->next;
-        cur->next = pre;
-
-    }
-
-    head->next = cur;
-
-}
-
-
-bool del_node(struct Node *head, const double score, const double point) {
-
-    struct Node *ptr, *pre;
-    ptr = head->next;
-    pre = head;
-
-    while(ptr) {
-
-        if(ptr->score == score && ptr->point == point) {
-
-            pre->next = ptr->next;
-            return true;
-        }
-
-        pre = ptr;
-        ptr = ptr->next;
-    }
-
-    return false;
-
-}
-
-
-bool input(double &score, double &point, stringstream &ss) {
+bool input(double &score, int &point, stringstream &ss) {
 
     string input;
     bool first = true;
@@ -148,6 +64,11 @@ public:
     Grade() {
 
         head = new Node;
+
+        head->score = -1;
+        head->point = -1;
+        head->next = NULL;
+
         course = 0;
         score_sum = 0;
         point_sum = 0;
@@ -254,19 +175,15 @@ public:
 
 signed main() {
 
-    struct Node *head = new Node;
     Grade grade;
 
 
     stringstream input_ss;
     string cmd_tmp, cmd, tmp;
 
-    double score_sum, score, point_sum, point;
-    int course;
+    double score;
+    int point;
 
-
-
-    program_init(head, score_sum, point_sum, course);
 
     cout << "~Final Grade Cal~" << endl;
     cout << endl;
@@ -316,11 +233,11 @@ signed main() {
 
                     if(score >= 0 && point >= 0 && score <= 100) {
 
-                        add_node(head, score, point);
+                        grade.add_node(score, point);
 
-                        course += 1;
-                        score_sum += score * point;
-                        point_sum += point;
+                        grade.modifyCourse(1);
+                        grade.modifyScore(score * point);
+                        grade.modifyPoint(point);
 
                     } else if(score > 100) {
 
@@ -350,15 +267,15 @@ signed main() {
                     } while(score < 0 || point < 0 || score > 100);
                 }
 
-                add_node(head, score, point);
+                grade.add_node(score, point);
 
-                course += 1;
-                score_sum += score * point;
-                point_sum += point;
+                grade.modifyCourse(1);
+                grade.modifyScore(score * point);
+                grade.modifyPoint(point);
 
             } else if(cmd == "status") {
 
-                if(!calResult(course, score_sum, point_sum)) {
+                if( !grade.getAvg() ) {
 
                     cout << "-> No any grade to display" << endl;
                 }
@@ -378,13 +295,13 @@ signed main() {
 
                 }
 
-                if(del_node(head, score, point)) {
+                if( grade.del_node(score, point) ) {
 
                     cout << "-> Delete data success" << endl;
 
-                    course -= 1;
-                    score_sum -= score * point;
-                    point_sum -= point;
+                    grade.modifyCourse(-1);
+                    grade.modifyScore( -score * point);
+                    grade.modifyPoint( -point);
 
                 } else {
 
@@ -393,8 +310,9 @@ signed main() {
 
             } else if(cmd == "look") {
 
-                if(travel_node(head)) {
+                if( grade.travel_node() ) {
                     cout << "-> Grade list was empty" << endl;
+
                 }
 
             } else {
@@ -408,9 +326,10 @@ signed main() {
 
     cout << "-> Program finished" << endl;
 
-    if(!calResult(course, score_sum, point_sum)) {
 
+    if( !grade.getAvg() ) {
         cout << "-> No any grade to display" << endl;
+
     }
 
     system("pause");
